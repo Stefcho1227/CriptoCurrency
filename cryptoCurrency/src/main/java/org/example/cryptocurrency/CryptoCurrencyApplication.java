5b72cd38f0a12fdb4944823f9666967e542db88e
Stefcho1227 <stefan.ivanov1227@gmail.com>
2025-03-25 11:05:34 +0200

    Added Java project application

---
+++ cryptoCurrency/src/main/java/org/example/cryptocurrency/CryptoCurrencyApplication.java
@@ -0,0 +1,13 @@
+package org.example.cryptocurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyApplication.class, args);
+    }
+
+}
---
+++ cryptoCurrency/src/test/java/org/example/cryptocurrency/CryptoCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cryptocurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
